6bd409122f62ca02ebdbd5f4f873d26d4102dc8f
ryan(SON) <ryanp3@naver.com>
2025-10-13 15:55:27 +0900

    Title: 비교 연산, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251013/비교 연산/comparison-operator.py
+++ 251013/비교 연산/comparison-operator.py
@@ -1,33 +1,7 @@
 a,b=map(int,input().split())
-
-if a>=b:
-    print("1")
-else:
-    print("0")
-
-
-if a>b:
-    print("1")
-else:
-    print("0")
-
-if b>=a:
-    print("1")
-else:
-    print("0")
-
-if b>a:
-    print("1")
-else:
-    print("0")
-
-if a==b:
-    print("1")
-else:
-    print("0")
-
-
-if a!=b:
-    print("1")
-else:
-    print("0")
+print(int(a>=b))
+print(int(a>b))
+print(int(b>=a))
+print(int(b>a))
+print(int(a==b))
+print(int(a!=b))
