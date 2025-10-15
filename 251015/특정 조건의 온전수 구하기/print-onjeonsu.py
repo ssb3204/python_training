7cb9d7c32fa7b31784176e6a547670214b512f06
ryan(SON) <ryanp3@naver.com>
2025-10-15 17:07:42 +0900

    Title: 특정 조건의 온전수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251015/특정 조건의 온전수 구하기/print-onjeonsu.py
+++ 251015/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -1,7 +1,10 @@
 a=int(input())
 for i in range(1,a+1):
     if i%2==0:
+        continue
     elif i%10==5:
+        continue
     elif i%3==0 and i%9!=0:
+        continue
     else:
         print(i,end=" ")
