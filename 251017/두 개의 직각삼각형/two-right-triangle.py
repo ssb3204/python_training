4c6705946113303418a69e37d88eec060960f8ff
ryan(SON) <ryanp3@naver.com>
2025-10-17 15:50:00 +0900

    Title: 두 개의 직각삼각형, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251017/두 개의 직각삼각형/two-right-triangle.py
+++ 251017/두 개의 직각삼각형/two-right-triangle.py
@@ -4,7 +4,7 @@ for i in range(a,0,-1):
     for j in range(i):
         print("*",end="")
     for j in range((a-i)*2):
-        print(" ")
+        print(" ",end="")
     for j in range(i):
         print("*",end="")
     print()
