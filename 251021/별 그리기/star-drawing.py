61405ec1da03eabd158b404d018badd5b8ab0cb1
ryan(SON) <ryanp3@naver.com>
2025-10-21 18:49:08 +0900

    Title: 별 그리기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251021/별 그리기/star-drawing.py
+++ 251021/별 그리기/star-drawing.py
@@ -1,15 +1,15 @@
 a = int(input())
 
 # Please write your code here.
-for i in range(1,a+1):
+for i in range(1,a):
     for j in range(a-i):
         print(" ",end="")
     for j in range(i*2-1):
         print("*",end="")
     print()
-for i in range(a+1,1,-1):
+for i in range(a,0,-1):
     for j in range(i):
         print(" ",end="")
-    for j in range(i*2-1):
+    for j in range((i-1)*2-1):
         print("*",end="")
     print()
