0400651a32b917562cbe0293b7083407a08ca982
ryan(SON) <ryanp3@naver.com>
2025-10-26 21:47:29 +0900

    Title: 알파벳 출력하기 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251026/알파벳 출력하기 2/print-alphabet-2.py
+++ 251026/알파벳 출력하기 2/print-alphabet-2.py
@@ -3,7 +3,7 @@ num=65
 for i in range(a,0,-1):
     for j in range(a-i):
         print(" ",end=" ")
-    for j in range(a,0,-1):
+    for j in range(i,0,-1):
         if num==91:
             num=65
         print(chr(num),end=" ")
