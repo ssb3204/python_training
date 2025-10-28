6082e0b93d0b663ea6f1113e4c6d801cc94054e2
ryan(SON) <ryanp3@naver.com>
2025-10-28 14:41:12 +0900

    Title: 코로나 메뉴얼2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251028/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,18 @@
+ls=[0]*5
+
+for i in range(3):
+    a,b=map(str,input().split())
+    if a=="Y" and int(b)>=37:
+        ls[0]+=1
+    elif a=="N" and int(b)>=37:
+        ls[1]+=1
+    elif a=="Y" and int(b)<37: 
+        ls[2]+=1
+    else:
+        ls[3]+=1
+
+if ls[0]>=2:
+    ls[4]="E"
+
+for i in ls:
+    print(i,end=" ")
