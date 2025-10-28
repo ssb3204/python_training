faf263a36e5482f8ab4a01b63a15f81d75c35c2f
ryan(SON) <ryanp3@naver.com>
2025-10-28 15:12:09 +0900

    Title: 배열 놀이, Time: 83ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251028/배열 놀이/play-with-array.py
@@ -0,0 +1,18 @@
+a,b=map(int,input().split())
+
+ls= list(map(int,input().split()))
+
+for _ in range(a):
+    q=list(map(int,input().split()))
+    
+    if q[0]==1:
+        print(ls[q[1]-1])
+    elif q[0]==2:
+        if q[1] not in ls:
+            print("0")
+        else:
+            print(ls.index(q[1])+1)
+    elif q[0]==3:
+        for i in range(q[1]-1,q[2]):
+            print(ls[i],end=" ")
+        print()
