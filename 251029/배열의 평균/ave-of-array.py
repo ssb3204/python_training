f4b47a80bdbbc46662a45e6ef398371ca6830064
ryan(SON) <ryanp3@naver.com>
2025-10-29 20:01:01 +0900

    Title: 배열의 평균, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251029/배열의 평균/ave-of-array.py
@@ -0,0 +1,20 @@
+a=[0]*4
+b=[]
+
+for _ in range(2):
+    sum=0
+    ls=list(map(int,input().split()))
+    for i in range(4):
+        a[i]+=ls[i]
+        sum+=ls[i]
+    b.append(sum)
+
+for i in b:
+    print(f"{i/4:.1f}",end=" ")
+print()
+num=0
+for i in a:
+    print(f"{i/2:.1f}",end=" ")
+    num+=i
+print()
+print(f"{num/2:.1f}")
