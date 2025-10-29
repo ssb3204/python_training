2e87b9e94e289facb9d175adc5ba3f13f351d0b5
ryan(SON) <ryanp3@naver.com>
2025-10-29 20:41:13 +0900

    Title: 두 배열의 곱, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251029/두 배열의 곱/multiple-of-two-arrays.py
+++ 251029/두 배열의 곱/multiple-of-two-arrays.py
@@ -10,9 +10,6 @@ for _ in range(3):
     b.append(ls)
 k=0
 for i,j in zip(a,b):
-    if k==2:
-        k=0
-        print()
     num=([x*y for x,y in zip(i,j)])
     print(*num,end=" ")
-    k+=1
+    print()
