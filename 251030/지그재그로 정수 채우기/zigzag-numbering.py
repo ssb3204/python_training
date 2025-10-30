896a4c3be19aa591477ef1fd0825697a09f44adb
ryan(SON) <ryanp3@naver.com>
2025-10-30 12:00:31 +0900

    Title: 지그재그로 정수 채우기, Time: 70ms, Memory: 18MB, Status: Passed - Codetree

--- 251030/지그재그로 정수 채우기/zigzag-numbering.py
+++ 251030/지그재그로 정수 채우기/zigzag-numbering.py
@@ -3,13 +3,17 @@ n, m = map(int, input().split())
 ls=[[0 for _ in range(m)] for _ in range(n)]
 
 cnt=0
-print(ls)
 # Please write your code here.
-for i in range(n):
-    for j in range(m):
+for i in range(m):
+    for j in range(n):
         if i%2==0:
             ls[j][i]=cnt
             cnt+=1
         else:
             ls[n-j-1][i]=cnt
             cnt+=1
+
+for i in ls:
+    for j in i:
+        print(j,end=" ")
+    print()
