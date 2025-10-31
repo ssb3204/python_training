2f28de4f78703c0a956c86c4b6f51b6fbb0873f2
ryan(SON) <ryanp3@naver.com>
2025-10-31 17:29:35 +0900

    Title: Run Length 인코딩, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251031/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,22 @@
+A = input()
+ls=[]
+# Please write your code here.
+i=0
+while(i<len(A)):
+    c=A[i]
+    cnt=1
+    j=i+1
+    for k in range(i+1,len(A)):
+        if A[i]==A[k] and k<len(A):
+            cnt+=1
+            j+=1
+        else:
+            break
+    ls.append(c)
+    ls.append(cnt)
+    i=j
+
+
+a="".join(str(i) for i in ls)
+print(len(a),a,sep="\n")
+
