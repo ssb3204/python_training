21382a2f9243919c8977a76649d8e9cd9dcd49ba
ryan(SON) <ryanp3@naver.com>
2025-11-01 22:39:32 +0900

    Title: 정수만 추출하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251101/정수만 추출하기/extract-only-integers.py
+++ 251101/정수만 추출하기/extract-only-integers.py
@@ -1,15 +1,18 @@
 ls =list(map(str,input().split()))
 num=[]
-is_t=False
+
 for i in ls:
     for j in i:
+        is_t=False
         if not j.isdigit():
             is_t=True
+            a=""
+            b=""
             a,b=i.split(j)
             num.append(a)
             break
     if is_t==False:
-        num.append(j)
+        num.append(i)
         
     
 sum=0
@@ -17,7 +20,3 @@ for i in num:
     sum+=int(i)
 
 print(sum)
-
-
-
-
