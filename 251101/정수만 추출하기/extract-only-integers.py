ea23aa3c105e46facac154867d72cb8e91b08a5f
ryan(SON) <ryanp3@naver.com>
2025-11-01 22:32:29 +0900

    Title: 정수만 추출하기, Time: 79ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 251101/정수만 추출하기/extract-only-integers.py
+++ 251101/정수만 추출하기/extract-only-integers.py
@@ -1,10 +1,15 @@
 ls =list(map(str,input().split()))
 num=[]
+is_t=False
 for i in ls:
     for j in i:
         if not j.isdigit():
+            is_t=True
             a,b=i.split(j)
             num.append(a)
+    if is_t==False:
+        num.append(j)
+        
     
 sum=0
 for i in num:
