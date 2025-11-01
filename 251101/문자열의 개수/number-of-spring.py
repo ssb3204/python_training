cee2d329793678b8da0959b40922057c2b856dcf
ryan(SON) <ryanp3@naver.com>
2025-11-01 23:07:49 +0900

    Title: 문자열의 개수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251101/문자열의 개수/number-of-spring.py
+++ 251101/문자열의 개수/number-of-spring.py
@@ -2,13 +2,14 @@ cnt=0
 ls=[]
 while(True):
     a=input()
-    ls.append(a)
     if a=="0":
         break
     else:
+        ls.append(a)
         cnt+=1
     
-
 print(cnt)
-for i in range(0,len(ls),2):
-    print(ls[i])
+
+if len(ls)!=0:
+    for i in range(0,len(ls),2):
+        print(ls[i])
