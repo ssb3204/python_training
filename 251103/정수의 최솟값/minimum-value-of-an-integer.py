2000f139a85af06ef4861a45c168976264972c5a
ryan(SON) <ryanp3@naver.com>
2025-11-03 11:38:32 +0900

    Title: 정수의 최솟값, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251103/정수의 최솟값/minimum-value-of-an-integer.py
+++ 251103/정수의 최솟값/minimum-value-of-an-integer.py
@@ -3,13 +3,12 @@ a, b, c = map(int, input().split())
 # Please write your code here.
 
 def f(i,j,k):
-    if i<j and i<k:
+    if i<=j and i<=k:
         print(i)
-    elif j<i and j<k:
+    elif j<=i and j<=k:
         print(j)
-    elif k<i and k<j:
+    elif k<=i and k<=j:
         print(k)
-    else:
-        print(i)
+
 
 f(a,b,c)
