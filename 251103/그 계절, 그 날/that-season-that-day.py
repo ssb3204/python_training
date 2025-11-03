ece5e19461a8d24d34c2df5475e26a1048fc0233
ryan(SON) <ryanp3@naver.com>
2025-11-03 14:59:55 +0900

    Title: 그 계절, 그 날, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 251103/그 계절, 그 날/that-season-that-day.py
+++ 251103/그 계절, 그 날/that-season-that-day.py
@@ -2,51 +2,50 @@ Y, M, D = map(int, input().split())
 
 # Please write your code here.
 def yoon(a):
-    if a%100 and a%400!=0:
+    if a%4==0 and a%100==0 and a%400==0:
         return 1
-    elif a%4==0:
-        if a%100!=0:
-            return 0
-        else:
-            return 1
+    elif a%4==0 and a%100!=0:
+        return 1
+    else:
+        return 0
     
 
 def sesson(b):
-    if b>=3 and 5>=b:
+    if b in [3,4,5]:
         print("Spring")
-    elif b>=6 and 8>=b:
+    elif b in [6,7,8]:
         print("Summer")
-    elif b>=9 and 11>=b:
+    elif b in [9,10,11]:
         print("Fall")
-    else:
+    elif b in [12,1,2]:
         print("Winter")
 
-def ex(a,b):
-    if a in [1,3,5,7,8,10,12]:
-        if b<=31:
+def ex(a,b,c):
+    if b in [1,3,5,7,8,10,12]:
+        if c<=31:
             return True
         else:
             return False
-    elif a==2:
+    elif b==2:
         if yoon(a):
-            if b<=29:
+            if c<=29:
                 return True
             else:
                 return False
         else:
-            if b<=28:
+            if c<=28:
                 return True
             else:
                 return False
-    elif a in [2,4,6,9,11]:
-        if b<=30:
+    elif b in [4,6,9,11]:
+        if c<=30:
             return True
         else:
             return False
     else:
         return False
 
-if ex(M,D):
+if ex(Y,M,D):
     sesson(M)
 else:
     print("-1")
