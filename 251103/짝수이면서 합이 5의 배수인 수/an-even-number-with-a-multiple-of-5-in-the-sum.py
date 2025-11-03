b24684f924fb177607772312addd4dfc7bbcee17
ryan(SON) <ryanp3@naver.com>
2025-11-03 11:49:11 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251103/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
+++ 251103/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
@@ -2,9 +2,8 @@ n = int(input())
 
 # Please write your code here.
 def f(a):
-    if a%2==0:
-        if ((a%10)+(a//10))%5==0:
-            return 1
+    if a%2==0 and ((a%10)+(a//10))%5==0:
+        return 1
 if f(n):
     print("Yes")
 else:
