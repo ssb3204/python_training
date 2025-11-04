720b2b1e1dcd66b1ff20d89c24276c73d1e9580e
ryan(SON) <ryanp3@naver.com>
2025-11-04 21:18:15 +0900

    Title: 절댓값으로 변경, Time: 70ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251104/절댓값으로 변경/find-the-absolute-value.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+def f(a):
+    for i in a:
+        if i<0:
+            print(i*-1,end=" ")
+        else:
+            print(i,end=" 
+            ")
+
+
+f(arr)
