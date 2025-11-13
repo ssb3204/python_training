ec1de7ff46c9835fc65ec2fb12cd2a48af1e0907
ryan(SON) <ryanp3@naver.com>
2025-11-13 15:26:33 +0900

    Title: Date to Date, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251113/Date to Date/date-to-date.py
+++ 251113/Date to Date/date-to-date.py
@@ -3,10 +3,16 @@ m1, d1, m2, d2 = map(int, input().split())
 # Please write your code here.
 dom=[0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 sum=0
-sum+=dom[m1]-d1+d2+1
+if m1!=m2:
+    sum+=dom[m1]-d1+d2+1
 
+    for i in range(m1+1,m2):
+        sum+=dom[i]
+else:
+    if d2!=d1:
+        sum+=d2-d1
+    else:
+        sum+=1
 
-for i in range(m1+1,m2):
-    sum+=dom[m1]
 
 print(sum)
