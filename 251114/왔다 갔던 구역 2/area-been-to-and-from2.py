c86dad8cc3cf1ddc0c4f259371d649007ffe15fb
ryan(SON) <ryanp3@naver.com>
2025-11-14 20:21:09 +0900

    Title: 왔다 갔던 구역 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251114/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 251114/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -11,12 +11,12 @@ ls=[0 for _ in range(n*20)]
 now=n*10
 for i in range(n):
     if dir[i]=="R":
-        for j in range(now,now+int(x[i])):
-            ls[j-1]+=1
+        for j in range(now+1,now+int(x[i])+1):
+            ls[j]+=1
         now=now+int(x[i])
     elif dir[i]=="L":
-        for j in range(now,now-int(x[i]),-1):
-            ls[j-1]+=1
+        for j in range(now-1,now-int(x[i])-1,-1):
+            ls[j]+=1
         now=now-int(x[i])
 cnt=0
 for i in ls:
