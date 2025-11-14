785713ad8eb57d327c1825004cc3d9c914567113
ryan(SON) <ryanp3@naver.com>
2025-11-14 20:32:30 +0900

    Title: 왔다 갔던 구역 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251114/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 251114/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -11,7 +11,7 @@ ls=[0 for _ in range(n*20)]
 now=n*10
 for i in range(n):
     if dir[i]=="R":
-        for j in range(now+1,now+int(x[i])+1):
+        for j in range(now,now+int(x[i])):
             ls[j]+=1
         now=now+int(x[i])
     elif dir[i]=="L":
