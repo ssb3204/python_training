73a51d19a314f02d69b7211f48bbbf01c7d7f888
ryan(SON) <ryanp3@naver.com>
2025-11-17 14:50:50 +0900

    Title: 사각형의 총 넓이 2, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251117/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -0,0 +1,23 @@
+n = int(input())
+x1, y1, x2, y2 = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    x1.append(a)
+    y1.append(b)
+    x2.append(c)
+    y2.append(d)
+
+# Please write your code here.
+ls=[[0 for _ in range(10)]for _ in range(10)]
+
+for i in range(n):
+    for j in range(x1[i],x2[i]):
+        for k in range(y1[i],y2[i]):
+            ls[j][k]=1
+sum=0
+for i in ls:
+    for j in i:
+        if j==1:
+            sum+=1
+
+print(sum)
