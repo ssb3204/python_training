2d286e04d21178245d3054db79ef1ba4482dadf7
ryan(SON) <ryanp3@naver.com>
2025-11-17 16:55:29 +0900

    Title: 연속되는 수 2, Time: 63ms, Memory: 16MB, Status: Passed - Codetree

--- 251117/연속되는 수 2/continuous-number2.py
+++ 251117/연속되는 수 2/continuous-number2.py
@@ -3,19 +3,14 @@ arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 ls=[]
-sum=1
+sum=0
 num=0
 for i in range(n-1):
     if arr[i]==arr[i+1]:
         num+=1
     else:
-        ls.append(num)
         num=0
-ls.append(num)
+    sum=max(sum,num)
 
-if len(ls)==0:
-    print(sum)
-else:
-    ls.sort(reverse=True)
-    print(sum+ls[0])
+print(sum+1)
         
