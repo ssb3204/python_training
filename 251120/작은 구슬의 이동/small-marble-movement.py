14506dc495a1810ca4b17fe45a99f4f02bcc7a07
ryan(SON) <ryanp3@naver.com>
2025-11-20 14:46:25 +0900

    Title: 작은 구슬의 이동, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251120/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,30 @@
+n, t = map(int, input().split())
+r, c, d = input().split()
+r, c = int(r), int(c)
+
+# Please write your code here.
+dx=[0,1,-1,0]
+dy=[1,0,0,-1]
+
+def in_dex(x,y):
+    return 0<=x and x<n and 0<=y and y<n
+
+def check_dir(d):
+    if d=='R':
+        return 0
+    elif d=='L':
+        return 3
+    elif d=='U':
+        return 2
+    elif d=='D':
+        return 1
+
+d=check_dir(d)
+
+for i in range(t+1):
+    x,y=r+dx[d],c+dy[d]
+    if not in_dex(x,y):
+        d=3-d
+    r,c=r+dx[d],c+dy[d]
+
+print(r,c,sep=" ")
