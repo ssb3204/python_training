d5dca8f0bd0d59c83abfa5aca0d4bdcaf4cd30a9
ryan(SON) <ryanp3@naver.com>
2025-11-20 15:14:22 +0900

    Title: 작은 구슬의 이동, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251120/작은 구슬의 이동/small-marble-movement.py
+++ 251120/작은 구슬의 이동/small-marble-movement.py
@@ -7,7 +7,7 @@ dx=[0,1,-1,0]
 dy=[1,0,0,-1]
 
 def in_dex(x,y):
-    return 0<=x and x<n and 0<=y and y<n
+    return 0<x and x<=n and 0<y and y<=n
 
 def check_dir(d):
     if d=='R':
@@ -21,10 +21,12 @@ def check_dir(d):
 
 d=check_dir(d)
 
-for i in range(t+1):
+for _ in range(t):
     x,y=r+dx[d],c+dy[d]
     if not in_dex(x,y):
         d=3-d
+        continue
     r,c=r+dx[d],c+dy[d]
+    
 
 print(r,c,sep=" ")
