b380d7affc3ca4510e9c6b9536fedc6b83b15ef5
ryan(SON) <ryanp3@naver.com>
2025-11-21 15:10:41 +0900

    Title: 격자 위의 편안한 상태, Time: 66ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251121/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 251121/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -3,7 +3,7 @@ ls =[[0 for _ in range(n)] for _ in range(n)]
 
 # Please write your code here.
 def in_dex(x,y):
-    return 0<=x and x<n-1 and 0<=y and y<n-1
+    return 1<=x and x<n-1 and 1<=y and y<n-1
 
 
 def check(x,y):
