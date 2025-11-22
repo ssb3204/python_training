73327b6bbf0ce1c96eb4f4bb8f0d0e85b1e3911e
ryan(SON) <ryanp3@naver.com>
2025-11-22 13:44:08 +0900

    Title: 격자 위의 편안한 상태, Time: 137ms, Memory: 20MB, Status: Passed - Codetree

--- 251122/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 251122/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -2,74 +2,20 @@ n, m = map(int, input().split())
 ls =[[0 for _ in range(n)] for _ in range(n)]
 
 # Please write your code here.
- 
-def in_dex(x,y):
-    return 1<=x and x<n-1 and 1<=y and y<n-1
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-def ze(x,y):
-    return (x==0 and y==0) or (x==0 and y==n-1) or (x==n-1 and y==0) or(x==n-1 and y==n-1)
+def check(x, y):
+    cnt = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
+        if 0 <= nx < n and 0 <= ny < n:
+            if ls[nx][ny] == 1:
+                cnt += 1
 
-def check(x,y):
-    cnt=0
-    if in_dex(x,y):
-        if ls[x+1][y]==1:
-            cnt+=1
-        if ls[x][y+1]==1:
-            cnt+=1
-        if ls[x-1][y]==1:
-            cnt+=1
-        if ls[x][y-1]==1:
-            cnt+=1
-        if cnt==3:
-            return 1
-        else:
-            return False
-    else:
-        if x==0 and y!=0:
-            if ls[x-1][y]==1:
-                cnt+=1
-            if ls[x][y+1]==1:
-                cnt+=1
-            if ls[x+1][y]==1:
-                cnt+=1
-            if cnt==3:
-                return 1
-            else:
-                return 0
-        elif y==0 and x!=0:
-            if ls[x+1][y]==1:
-                cnt+=1
-            if ls[x][y+1]==1:
-                cnt+=1
-            if ls[x][y-1]==1:
-                cnt+=1
-            if cnt==3:
-                return 1
-            else:
-                return 0
-        elif y==n-1 and x!=n-1:
-            if ls[x-1][y]==1:
-                cnt+=1
-            if ls[x][y+1]==1:
-                cnt+=1
-            if ls[x][y-1]==1:
-                cnt+=1
-            if cnt==3:
-                return 1
-            else:
-                return 0
-        elif x==n-1 and (y!=0 and y!=n-1):
-            if ls[x-1][y]==1:
-                cnt+=1
-            if ls[x][y-1]==1:
-                cnt+=1
-            if ls[x+1][y]==1:
-                cnt+=1
-            if cnt==3:
-                return 1
-            else:
-                return 0
+    return cnt == 3
 
 
 
