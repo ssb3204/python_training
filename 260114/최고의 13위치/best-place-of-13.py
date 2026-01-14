66d4238c6c427c3237a5db4b45bf9c9f3b8c71c9
ryan(SON) <ryanp3@naver.com>
2026-01-14 16:41:06 +0900

    Title: 최고의 13위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260114/최고의 13위치/best-place-of-13.py
+++ 260114/최고의 13위치/best-place-of-13.py
@@ -1,8 +1,10 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
+ma=0
 # Please write your code here.
-
 for i in range(n):
-    for j in range(n):
-        print(gird[i][j])
+    for j in range(n-2):
+        ma= max(ma,grid[i][j]+grid[i][j+1]+grid[i][j+2])
+    
+print(ma)
