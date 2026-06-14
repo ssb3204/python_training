f1737f434cdc97b4e11689bc17ea1dea4f8e8a42
ryan(SON) <ryanp3@naver.com>
2026-06-14 21:42:24 +0900

    [Passed] A, B, C, D 찾기 2 (104ms, 17MB)

--- trail2/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ trail2/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -5,22 +5,7 @@ for i in range(1,41):
     for j in range(i,41):
         for k in range(j,41):
             for x in range(k,41):
-                ls=[]
-                ls.append(i)
-                ls.append(j)
-                ls.append(k)
-                ls.append(x)
-                ls.append(i+j)
-                ls.append(j+k)
-                ls.append(k+x)
-                ls.append(x+i)
-                ls.append(i+k)
-                ls.append(j+x)
-                ls.append(i+j+k)
-                ls.append(i+j+x)
-                ls.append(i+k+x)
-                ls.append(j+k+x)
-                ls.append(i+j+k+x)
+                ls=[i,j,k,x,i+j,j+k,k+x,x+i,i+k,j+x,i+j+k,i+j+x,i+k+x,j+k+x,i+j+k+x]
                 ls.sort()
                 if ls==nums:
                     print(i,j,k,x,sep=" ")
